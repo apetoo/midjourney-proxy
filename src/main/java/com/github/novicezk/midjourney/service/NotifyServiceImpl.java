2a3cf9b505570c723acb886f10b3aa7ea52f69c1
apeto <warape@163.com>
2023-08-22 13:55:15 +0800

    修改

--- src/main/java/com/github/novicezk/midjourney/service/NotifyServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/NotifyServiceImpl.java
@@ -5,29 +5,22 @@ import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.exceptions.CheckedUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import cn.hutool.core.util.StrUtil;
-import cn.hutool.json.JSONUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.novicezk.midjourney.Constants;
 import com.github.novicezk.midjourney.ProxyProperties;
 import com.github.novicezk.midjourney.enums.TaskStatus;
 import com.github.novicezk.midjourney.support.Task;
-import com.qcloud.cos.model.ObjectMetadata;
-import com.qcloud.cos.model.PutObjectRequest;
-import com.qcloud.cos.model.UploadResult;
 import com.qcloud.cos.transfer.TransferManager;
-import com.qcloud.cos.transfer.Upload;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.HttpUrl;
 import org.springframework.http.*;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Service
@@ -61,7 +54,6 @@ public class NotifyServiceImpl implements NotifyService {
 			this.executor.execute(() -> {
 				synchronized (taskLock) {
 					try {
-						updateCosTask(task);
                         String paramsStr = OBJECT_MAPPER.writeValueAsString(task);
 						ResponseEntity<String> responseEntity = postJson(notifyHook, paramsStr);
 						if (responseEntity.getStatusCode() == HttpStatus.OK) {
@@ -86,77 +78,15 @@ public class NotifyServiceImpl implements NotifyService {
 
   @Override
     public void updateCosTask(Task task) {
-        String taskId = task.getId();
-        URL url;
-        URLConnection connection;
         String imageUrl = task.getImageUrl();
         log.info(imageUrl);
         if (StrUtil.isBlank(imageUrl)) {
             return;
         }
-        try {
-            url = new URL(imageUrl);
-            connection = url.openConnection();
-        } catch (Exception e) {
-            log.error("根据URL地址获取图片异常 地址:{}", imageUrl, e);
-            return;
-        }
-        try (InputStream inputStream = connection.getInputStream()) {
-            ProxyProperties.CosConfig cosConfig = properties.getCosConfig();
-            String extension = getExtension(url);
-            String key = taskId + "." + extension;
-            task.setCosImageUrl(cosConfig.getDomain() + "/" + key);
+      HttpUrl httpUrl = HttpUrl.get(imageUrl);
+      String url= httpUrl.newBuilder().host("cdn.discord.warape.top").build().url().toString();
+      task.setCosImageUrl(url);
 
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentDisposition("inline");
-            objectMetadata.setContentType("image/" + extension);
-//            objectMetadata.setContentLength();
-            Map<String, String> userMetadata = new HashMap<>();
-//            userMetadata.put("id", taskId);
-//            userMetadata.put("name", key);
-            userMetadata.put("taskStatus", task.getStatus().name());
-//      userMetadata.put("failReason", task.getFailReason());
-            userMetadata.put("notifyHook", task.getPropertyGeneric(Constants.TASK_PROPERTY_NOTIFY_HOOK));
-            userMetadata.put("relatedTaskId", task.getPropertyGeneric(Constants.TASK_PROPERTY_RELATED_TASK_ID));
-            userMetadata.put("state", task.getState());
-            objectMetadata.setUserMetadata(userMetadata);
-
-            PutObjectRequest putObjectRequest = new PutObjectRequest(cosConfig.getBucketName(), key, inputStream, objectMetadata);
-            Upload upload = transferManager.upload(putObjectRequest);
-            log.info("upload:{}", JSONUtil.toJsonStr(upload));
-            UploadResult uploadResult = upload.waitForUploadResult();
-            log.info("uploadResult:{}", JSONUtil.toJsonStr(uploadResult));
-        } catch (Exception e) {
-            log.error("cos上传异常", e);
-        }
     }
-
-  public static String getExtension (URL url) {
-    String path = url.getPath();
-    int lastDotPos = path.lastIndexOf('.');
-
-    // 如果找到了'.'，则截取其后的部分作为文件扩展名；否则，文件扩展名为空
-    return lastDotPos != -1 ? path.substring(lastDotPos + 1) : "";
-  }
-
-//    public static void main(String[] args) throws Exception {
-//        URL url = new URL("https://cdn.discordapp.com/attachments/1114559240116371456/1114840474742685696/1114840427066052660.png");
-//
-//        String extension = getExtension(url);
-//
-//        URLConnection connection = url.openConnection();
-//        InputStream inputStream = connection.getInputStream();
-//        String key = "123" + "." + extension;
-//        ObjectMetadata metadata = new ObjectMetadata();
-//        metadata.setContentDisposition("inline");
-//        metadata.setContentType("image/" + extension);
-//        PutObjectRequest putObjectRequest = new PutObjectRequest("bj-1259324451", key, inputStream, metadata);
-//        putObjectRequest.withMetadata(metadata);
-//        Upload upload = CosUploadUtils.createTransferManager("", "").upload(putObjectRequest);
-//
-//        System.out.println(JSONUtil.toJsonStr(upload));
-//        UploadResult uploadResult = upload.waitForUploadResult();
-//        System.out.println(JSONUtil.toJsonStr(uploadResult));
-//    }
 
 }
--- src/main/java/spring/config/BeanConfig.java
+++ src/main/java/spring/config/BeanConfig.java
@@ -11,14 +11,6 @@ import com.github.novicezk.midjourney.service.translate.NoTranslateServiceImpl;
 import com.github.novicezk.midjourney.support.Task;
 import com.github.novicezk.midjourney.wss.WebSocketStarter;
 import com.github.novicezk.midjourney.wss.user.UserWebSocketStarter;
-import com.qcloud.cos.COSClient;
-import com.qcloud.cos.ClientConfig;
-import com.qcloud.cos.auth.BasicCOSCredentials;
-import com.qcloud.cos.auth.COSCredentials;
-import com.qcloud.cos.http.HttpProtocol;
-import com.qcloud.cos.region.Region;
-import com.qcloud.cos.transfer.TransferManager;
-import com.qcloud.cos.transfer.TransferManagerConfiguration;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,8 +20,6 @@ import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @Configuration
 public class BeanConfig {
@@ -71,71 +61,5 @@ public class BeanConfig {
 	@Bean
 	ApplicationRunner enableMetaChangeReceiverInitializer(WebSocketStarter webSocketStarter) {
 		return args -> webSocketStarter.start();
-	}
-
-	@Bean
-	// 创建 TransferManager 实例，这个实例用来后续调用高级接口
-	public TransferManager createTransferManager(COSClient cosClient) {
-
-		// 创建一个 COSClient 实例，这是访问 COS 服务的基础实例。
-		// 详细代码参见本页: 简单操作 -> 创建 COSClient
-//        COSClient cosClient = createCOSClient();
-
-		// 自定义线程池大小，建议在客户端与 COS 网络充足（例如使用腾讯云的 CVM，同地域上传 COS）的情况下，设置成16或32即可，可较充分的利用网络资源
-		// 对于使用公网传输且网络带宽质量不高的情况，建议减小该值，避免因网速过慢，造成请求超时。
-		ExecutorService threadPool = Executors.newFixedThreadPool(32);
-
-		// 传入一个 threadpool, 若不传入线程池，默认 TransferManager 中会生成一个单线程的线程池。
-		TransferManager transferManager = new TransferManager(cosClient, threadPool);
-
-		// 设置高级接口的配置项
-		// 分块上传阈值和分块大小分别为 5MB 和 1MB
-		TransferManagerConfiguration transferManagerConfiguration = new TransferManagerConfiguration();
-		transferManagerConfiguration.setMultipartUploadThreshold(3 * 1024 * 1024);
-		transferManagerConfiguration.setMinimumUploadPartSize(1 * 1024 * 1024);
-		transferManagerConfiguration.setMultipartCopyThreshold(3 * 1024 * 1024);
-		transferManager.setConfiguration(transferManagerConfiguration);
-
-		return transferManager;
-	}
-
-	@Bean
-	// 创建 COSClient 实例，这个实例用来后续调用请求
-	public COSClient createCOSClient(ProxyProperties properties) {
-		ProxyProperties.CosConfig cosConfig = properties.getCosConfig();
-
-
-//        String secretId = SpringUtil.getProperty("secretId");
-//        String secretKey = SpringUtil.getProperty("secretKey");
-		// 设置用户身份信息。
-		// SECRETID 和 SECRETKEY 请登录访问管理控制台 https://console.cloud.tencent.com/cam/capi 进行查看和管理
-		COSCredentials cred = new BasicCOSCredentials(cosConfig.getSecretId(), cosConfig.getSecretKey());
-
-		// ClientConfig 中包含了后续请求 COS 的客户端设置：
-		ClientConfig clientConfig = new ClientConfig();
-
-		// 设置 bucket 的地域
-		// COS_REGION 请参见 https://cloud.tencent.com/document/product/436/6224
-		clientConfig.setRegion(new Region("ap-beijing"));
-
-		// 设置请求协议, http 或者 https
-		// 5.6.53 及更低的版本，建议设置使用 https 协议
-		// 5.6.54 及更高版本，默认使用了 https
-		clientConfig.setHttpProtocol(HttpProtocol.https);
-		String cosSuffix = "cos.accelerate.myqcloud.com";
-		clientConfig.setEndPointSuffix(cosSuffix);
-		// 以下的设置，是可选的：
-
-		// 设置 socket 读取超时，默认 30s
-		clientConfig.setSocketTimeout(30 * 1000);
-		// 设置建立连接超时，默认 30s
-		clientConfig.setConnectionTimeout(30 * 1000);
-
-		// 如果需要的话，设置 http 代理，ip 以及 port
-//        clientConfig.setHttpProxyIp("httpProxyIp");
-//        clientConfig.setHttpProxyPort(80);
-
-		// 生成 cos 客户端。
-		return new COSClient(cred, clientConfig);
 	}
 }
